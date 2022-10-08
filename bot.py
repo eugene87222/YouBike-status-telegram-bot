9f03543dd86fab6f8906ec8ba22bd589e6287ef0
Eugene Yang <eugene87222@gmail.com>
2022-10-08 23:05:54 +0800

    update

--- bot.py
+++ bot.py
@@ -13,11 +13,11 @@ dp = Dispatcher(bot)
     commands=['start', 'help'],
     chat_type=[ChatType.PRIVATE])
 async def welcome(message: types.Message):
-    msg = '*用途*：查詢 YouBike 站點狀態\n\n'
+    msg = '*用途*：查詢 YouBike 場站狀態\n\n'
     msg += '*使用方法一*：輸入一個或多個關鍵字查詢，關鍵字間用空白分隔\n'
     msg += '例如：`清華大學`、`交通大學 大學路`\n'
-    msg += '查詢的欄位有：\n- 站點名稱\n- 站點所在區域\n- 站點地址\n\n'
-    msg += f'*使用方法二*：使用 Telegram 傳送 GPS 座標，將回傳方圓 {get_radius(None)} 公尺內最近的五個站的站點狀態\n\n'
+    msg += '查詢的欄位有：\n- 場站名稱\n- 場站地址\n\n'
+    msg += f'*使用方法二*：使用 Telegram 傳送 GPS 座標，將回傳方圓 {get_radius(None)} 公尺內最近的五個場站的狀態\n\n'
     msg += '*其他*\n'
     msg += '可以利用 `/radius <距離>` 修改 GPS 搜尋半徑，預設為 600 公尺\n'
     msg += '例如：`/radius 500`（單位為公尺）'
@@ -37,8 +37,12 @@ async def radius(message: types.Message):
     elif not args.isdigit():
         await message.answer('距離只能是大於等於零的整數')
     else:
-        save_radius(message.from_user.id, int(args))
-        await message.answer(f'搜尋半徑已更新為 {int(args)} 公尺')
+        r = int(args)
+        if r > 20000000:
+            await message.answer(f'地球周長也就 40000 公里左右，你設那麼大幹嘛')
+        r = min(r, 20000000)
+        save_radius(message.from_user.id, r)
+        await message.answer(f'搜尋半徑已更新為 {r} 公尺')
 
 
 @dp.message_handler(
@@ -56,7 +60,7 @@ async def search(message: types.Message):
         await message.answer_dice(message.dice.emoji)
 
     elif message.content_type == ContentType.LOCATION:
-        status = await message.answer(f'正在取得方圓 {get_radius(message.from_user.id)} 公尺內最近的五個 YouBike 站點資訊')
+        status = await message.answer(f'正在取得方圓 {get_radius(message.from_user.id)} 公尺內最近的五個 YouBike 場站資訊')
         youbike1_stations = get_all_stations(youbike_type=1)
         youbike2_stations = get_all_stations(youbike_type=2)
         coor = {
@@ -80,7 +84,7 @@ async def search(message: types.Message):
         await message.answer_sticker(message.sticker.file_id)
 
     elif message.content_type == ContentType.TEXT:
-        status = await message.answer('正在取得 YouBike 站點資訊')
+        status = await message.answer('正在取得 YouBike 場站資訊')
         youbike1_stations = get_all_stations(youbike_type=1)
         youbike2_stations = get_all_stations(youbike_type=2)
         kw_list = [t.strip() for t in message.text.strip().split(' ')]
--- youbike.py
+++ youbike.py
@@ -24,10 +24,8 @@ def save_radius(user_id, radius):
 
 
 def distance(lat1, lon1, lat2, lon2): 
-    lon1 = radians(lon1)
-    lat1 = radians(lat1)
-    lon2 = radians(lon2)
-    lat2 = radians(lat2)
+    lon1, lat1 = radians(lon1), radians(lat1)
+    lon2, lat2 = radians(lon2), radians(lat2)
     dlon = lon2 - lon1  
     dlat = lat2 - lat1 
     dis = 6371 * 2 * asin(sqrt(sin(dlat/2)**2 + cos(lat1)*cos(lat2)*sin(dlon/2)**2))
@@ -51,7 +49,7 @@ def generate_formatted_output(results):
             res += f' ({dis:.1f}m)'
         res += f'\n[See on Google Map](https://www.google.com/maps/search/?api=1&query={lat:.6f},{lng:.6f})\n'
         res += f'地點：{dist} - {addr}\n'
-        res += f'可借/總數：*{avail_space}/{total_space}*\n'
+        res += f'可借/可還/總數：*{avail_space}/{empty_space}/{total_space}*\n'
         res += f'_updated at {update_time}_'
         output.append(res)
     return '\n\n\n'.join(output)
